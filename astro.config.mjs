f40b77b65769b62102939641859f2fe7434a16f5
AirbnbEcoPlus <adf7bn@gmail.com>
2023-12-14 19:28:52 +0100

    routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   ViewTransitions: true,
-  output: "server",
-  adapter: vercel(),
   integrations: [tailwind(), react()],
   site: 'https://airbnbecoplus.github.io',
   base: '/sprints',
